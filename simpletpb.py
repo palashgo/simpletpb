9ccd387c4874590ce0d8a1d306d55de6408ba596
Pablo <pablo.riutort@gmail.com>
2018-01-26 13:44:41 +0100

    Refactor get_magnets
    Makes use of map and filter functions and removes limit param in get_magnets
    Rewrite doc of get_magnets and search

--- simpletpb.py
+++ simpletpb.py
@@ -13,14 +13,8 @@ def search(query, category=category.ALL, order_by=order_by.DEFAULT, page=0):
     Makes a Request.get() to thepiratebay.org and parses the response text
     with BeautifulSoup
 
-    Args:
-        query: a query to search
-        category: optional value. Pirate Bay category param to put into the URL
-        order_by: optional value. Pirate Bay order param to put into the URL
-        category: optional value. Pirate Bay page param to put into the URL
-
     Returns:
-        beautiful soup parsed html
+        beautiful soup parsed HTML
     """
     url = 'https://thepiratebay.org/search/'
     url += quote_plus(query) + '/' + str(page) + '/' + str(order_by) + '/' + str(category)
@@ -28,31 +22,17 @@ def search(query, category=category.ALL, order_by=order_by.DEFAULT, page=0):
     return bs(get(url).text, 'html.parser')
 
 
-def get_magnets(bs_html, limit=1):
+def get_magnets(bs_html):
     """Gets magnet links from HTML
     
-    Searches for the srting "magnet" into attribute href of tags <a> from given HTML.
-    Stores the link if the string matches
+    Searches for the string "magnet" into attribute href of <a> tags from given HTML
+    and returns the occurrences as a list
     
     Args:
-        bs_html: beautiful soup parsed html
-        limit: An optional variable that determines the length of the
-            returned list
-    
-    Returns:
-        A list of magnets of length = limit found into the bs_html or None
+        bs_html: beautiful soup parsed HTML
     """
-    magnets = []
-
-    for link in bs_html.find_all('a'):
-        href = link.get('href')
-        if 'magnet' in href:
-            magnets.append(href)
-            limit -= 1
-            if limit == 0:
-                return magnets
-
-    return None
+    hrefs = list(map(lambda link: link.get('href'), bs_html.find_all('a')))
+    return list(filter(lambda href: href.startswith('magnet'), hrefs))
 
 
 if __name__ == '__main__':
@@ -63,3 +43,5 @@ if __name__ == '__main__':
             print(magnets[0])
         else:
             print('No magnets found')
+
+
