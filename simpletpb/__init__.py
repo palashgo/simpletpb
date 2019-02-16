1a2a53140a634493a182d8c86c64d510087b3991
Palash Golecha <palash.golecha2016@vitstudent.ac.in>
2019-02-16 14:57:47 +0530

    Detailed result

--- simpletpb/__init__.py
+++ simpletpb/__init__.py
@@ -5,33 +5,39 @@ from urllib.parse import quote_plus
 from bs4 import BeautifulSoup as bs
 from .categories import ALL
 from .order_by import DEFAULT
+import re
 
+class Torrent:
+    def __init__(self,title,seeds,peers,size,magnet,uploaded_time):
+        self.title = title
+        self.seeds = seeds
+        self.peers = peers
+        self.size  = size
+        self.magnet = magnet
+        self.uploaded_time = uploaded_time
 
-def search(query, category=ALL, order_by=DEFAULT, page=0):
-    """Query to The Pirate Bay
+class TPBSearch:
+    def __init__(self,query, category=ALL, order_by=DEFAULT, page=0):
+        self.soup = self._fetch(query, category, order_by, page)
+        self.result = self._parse(self.soup)
     
-    Makes a Request.get() to thepiratebay.org and parses the response text
-    with BeautifulSoup
+    def _fetch(self,query, category, order_by, page):
+        url = 'https://thepiratebay.org/search/'
+        url += quote_plus(query) + '/' + str(page) + '/' + str(order_by) + '/' + str(category)
+        print(url)
+        return bs(get(url).text, 'html.parser')
 
-    Returns:
-        beautiful soup parsed HTML
-    """
-    url = 'https://thepiratebay.org/search/'
-    url += quote_plus(query) + '/' + str(page) + '/' + str(order_by) + '/' + str(category)
-    print(url)
-    return bs(get(url).text, 'html.parser')
+    def _parse(self,soup):
+        result = []
+        for tag in soup.select("#searchResult tr")[1:]:
+            title = tag.select_one("a.detLink").text
+            seeds = tag.select_one("td:nth-child(3)").text
+            peers = tag.select_one("td:nth-child(4)").text
+            desc = tag.select_one("font.detDesc").text
+            magnet = tag.select_one("a[title=\"Download this torrent using magnet\"]").attrs.get('href')
+            size = re.search("Size\s(.+?),", desc).group(1).replace('\xa0',' ')
+            uploaded_time = re.search("Uploaded\s(.+?),", desc).group(1).replace('\xa0',' ')
+            tr_torrent = Torrent(title,seeds,peers,size,magnet,uploaded_time)
+            result.append(tr_torrent)
 
-
-def get_magnets(bs_html):
-    """Gets magnet links from HTML
-    
-    Searches for the string "magnet" into attribute href of <a> tags from given HTML
-    and returns the occurrences as a list
-    
-    Args:
-        bs_html: beautiful soup parsed HTML
-    """
-    hrefs = list(map(lambda link: link.get('href'), bs_html.find_all('a')))
-    return list(filter(lambda href: href.startswith('magnet'), hrefs))
-
-
+        return result
